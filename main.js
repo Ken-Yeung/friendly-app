f9f00e5f117241700de68d1a43a9f41c70e4f2e0
Ken-Yeung <yeungchunsiu@gmail.com>
2021-05-09 22:30:53 +0800

    9/5/2021 update

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function add_events(){
+    let sign_up_norm = document.getElementById("create_ac_portal");
+
+    sign_up_norm.addEventListener("click", (e) => {
+        alert("Sign Up");
+    });
+    return false;
+}
+
 $(document).ready(() => {
     console.log("Finished");
 });
