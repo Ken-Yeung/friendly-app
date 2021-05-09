a2a5fe8abdfe7694b160719ace68cbe1980ac744
Ken-Yeung <yeungchunsiu@gmail.com>
2021-05-09 22:43:51 +0800

    9/5/2021 update

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ function add_events(){
     let sign_up_norm = document.getElementById("create_ac_portal");
     let sign_in_port = document.getElementById("sign_in_portal");
 
-    sign_up_norm.addEventListener("click", sign_up_norm);
-    sign_in_port.addEventListener("click", sign_in_port);
+    sign_up_norm.addEventListener("click", sign_up_norm_fuc);
+    sign_in_port.addEventListener("click", sign_in_port_fuc);
 
     return false;
 }
 
-function sign_up_norm(e){
+function sign_up_norm_fuc(e){
     alert("Sign Up");
     return false;
 }
 
-function sign_in_port(e){
+function sign_in_port_fuc(e){
     alert("Sign In");
     return false;
 }
