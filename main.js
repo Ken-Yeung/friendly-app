343bf0707f1400dad716790e9a248dace2c59239
Ken-Yeung <yeungchunsiu@gmail.com>
2021-05-11 12:51:28 +0800

    11/5/2021 update

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function add_events(){
-    const sign_up_norm = document.getElementById("create_ac_portal");
+    const sign_up_norm = document.querySelectorAll("#create_ac_portal");
     const sign_in_port = document.querySelectorAll("#sign_in_portal");
 
-    sign_up_norm.addEventListener("click", sign_up_norm_fuc);
+    for (let i = 0; i < sign_up_norm.length; i++){
+        sign_up_norm[i].addEventListener("click", sign_up_norm_fuc);
+    }
 
     for (let i = 0; i < sign_in_port.length; i++){
         sign_in_port[i].addEventListener("click", sign_in_port_fuc);
