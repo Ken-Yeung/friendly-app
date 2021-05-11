9b68f7f7ad3f4568b2263a80bd6258b0988b6abe
Ken-Yeung <yeungchunsiu@gmail.com>
2021-05-11 20:32:35 +0800

    11/5/2021 update

--- main.js
+++ main.js
@@ -131,9 +131,9 @@ function end_click(e){
         let class_status = this.classList.contains("selecting");
         // alert(`${this_foo_id[0]}: ${this_foo_id[1]}`);
         if (!class_status){
-            $(this).addClass("selecting", 666, "linear");
+            $(this).children[0].addClass("selecting", 666, "linear");
         } else {
-            $(this).removeClass("selecting", 666, "linear");
+            $(this).children[0].removeClass("selecting", 666, "linear");
         }
     }
     // Clear timeout
