28b653456ecdd79e999594c854382777a3788a58
Ken-Yeung <yeungchunsiu@gmail.com>
2021-05-25 20:01:22 +0800

    update test_api.js

--- test_api.js
+++ test_api.js
@@ -2,15 +2,17 @@ function takeshot() {
     let div = document.getElementById('save_me');
     html2canvas(div, {
         dpi: 144,
-        onrendered: function (canvas) {
-            let src = canvas.toDataURL("image/jpeg", .9);
-            console.log(src);
-            let img = document.createElement("img");
-            img.src = src;
-            img.className = "test-img";
-            document.body.appendChild(img);
-        }
+        onrendered: myRenderFunction
     });
+}
+
+function myRenderFunction(canvas){
+    let src = canvas.toDataURL("image/jpeg", .9);
+    console.log(src);
+    let img = document.createElement("img");
+    img.src = src;
+    img.className = "test-img";
+    document.body.appendChild(img);
 }
 
 $(document).ready(()=>{
