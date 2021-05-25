80bd9f383177b5f96cdaa0a09f58fda6aab38537
Ken-Yeung <yeungchunsiu@gmail.com>
2021-05-25 20:04:29 +0800

    update test_api.js

--- test_api.js
+++ test_api.js
@@ -1,18 +1,14 @@
 function takeshot() {
     let div = document.getElementById('save_me');
-    html2canvas(div, {
-        dpi: 144,
-        onrendered: myRenderFunction
-    });
-}
-
-function myRenderFunction(canvas){
-    let src = canvas.toDataURL("image/jpeg", .9);
-    console.log(src);
-    let img = document.createElement("img");
-    img.src = src;
-    img.className = "test-img";
-    document.body.appendChild(img);
+    html2canvas(div).then(
+        function (canvas) {
+            let src = canvas.toDataURL("image/jpeg", .9);
+            console.log(src);
+            let img = document.createElement("img");
+            img.src = src;
+            img.className = "test-img";
+            document.body.appendChild(img);
+        });
 }
 
 $(document).ready(()=>{
