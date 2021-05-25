196c4876969bf74740d0cb0fec28768442dccfef
Ken-Yeung <yeungchunsiu@gmail.com>
2021-05-25 19:33:19 +0800

    update text_api

--- test_api.js
+++ test_api.js
@@ -1,3 +1,14 @@
+function takeshot() {
+    let div = document.getElementById('save_me');
+    html2canvas(div).then(
+        function (canvas) {
+            document.body.appendChild(canvas);
+        })
+}
+
 $(document).ready(()=>{
     console.log("Upload Successful!");
+    $("#takeshot").click(()=>{
+        takeshot();
+    });
 });
